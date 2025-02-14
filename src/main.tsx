fa9793944199265f939d9193763d90136e4ad087
shiba-tt <tatsuya1006cube@icloud.com>
2025-02-14 14:26:55 +0900

    add stylex

--- src/main.tsx
+++ src/main.tsx
@@ -2,7 +2,8 @@ import { StrictMode } from 'react';
 
 import { createRoot } from 'react-dom/client';
 
-import './index.css';
+import './stylex.css';
+
 import App from './App.tsx';
 
 createRoot(document.getElementById('root')!).render(
--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,12 @@
 /// <reference types="vitest" />
 import react from '@vitejs/plugin-react-swc';
 import { defineConfig } from 'vite';
+import styleX from 'vite-plugin-stylex';
 import tsconfigPaths from 'vite-tsconfig-paths';
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), tsconfigPaths()],
+  plugins: [react(), styleX(), tsconfigPaths()],
   test: {
     environment: 'happy-dom',
     setupFiles: ['./src/testing/vitest-setup.ts'],
