ba1050c710af480b4d66f0dfe1c0cb2fc7aa306b
shiba-tt <tatsuya1006cube@icloud.com>
2025-02-14 13:48:36 +0900

    add vitest

--- vite.config.ts
+++ vite.config.ts
@@ -1,3 +1,4 @@
+/// <reference types="vitest" />
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react-swc';
 import tsconfigPaths from 'vite-tsconfig-paths';
@@ -5,4 +6,12 @@ import tsconfigPaths from 'vite-tsconfig-paths';
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tsconfigPaths()],
+  test: {
+    environment: 'happy-dom',
+    setupFiles: ['./src/testing/vitest-setup.ts'],
+    // スナップショットの保存先を設定
+    resolveSnapshotPath: (path, extension) => {
+      return path.replace('/src/', '/__snapshots__/') + extension;
+    },
+  },
 });
